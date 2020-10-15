ffc4d9399cd2dc3f4396c2e9fd959fdfac10503e
selvert <01102827@pw.edu.pl>
2020-10-15 23:13:34 +0200

    Changes to Jupyter and app

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from pyspark.sql import SparkSession
 
-logFile = "YOUR_SPARK_HOME/README.md"  # Should be some file on your system
+install_requires=[
+        'pyspark=={site.SPARK_VERSION}'
+]
+
+
+logFile = "README.md"  # Should be some file on your system
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 logData = spark.read.text(logFile).cache()
 
--- start.py
+++ start.py
@@ -1,7 +1,8 @@
 
 import pyspark
 from pyspark.sql import SparkSession
-
+from pyspark.sql.column import Column
+from pyspark.sql.functions import explode_outer, size, split, 
 
 spark = SparkSession \
 .builder \
@@ -20,7 +21,7 @@ linesWithSpark = textFile.filter(textFile.value.contains("Spark"))
 textFile.filter(textFile.value.contains("Spark")).count()
 
 
-textFile.select(size(split(textFile.value, "\s+")).name("numWords")).agg(max(column("numWords"))).collect()
+textFile.select(size(split(textFile.value, "\s+")).name("numWords")).agg(max(Column("numWords"))).collect()
 
 wordCounts = textFile.select(explode_outer(split(textFile.value, "\s+")).alias("word")).groupBy("word").count()
 
