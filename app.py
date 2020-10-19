c3bdd2df581d0cf3002881c02f0c555da9ab5184
selvert <01102827@pw.edu.pl>
2020-10-19 23:43:59 +0200

    Test with functions

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ install_requires=[
 ]
 
 
-logFile = "README.md"  # Should be some file on your system
-spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
-logData = spark.read.text(logFile).cache()
+def init_spark():
+        logFile = "README.md"  # Should be some file on your system
+        spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+        sc = spark.sparkContext
+        logData = spark.read.text(logFile).cache()
+        return spark, sc, logData
 
-numAs = logData.filter(logData.value.contains('a')).count()
-numBs = logData.filter(logData.value.contains('b')).count()
-numCs = logData.filter(logData.value.contains('c')).count()
-print("Lines with a: %i, lines with b: %i, lines with c: %i" % (numAs, numBs, numCs))
+def main():
+        spark, sc, logData = init_spark()
+        numAs = logData.filter(logData.value.contains('a')).count()
+        numBs = logData.filter(logData.value.contains('b')).count()
+        numCs = logData.filter(logData.value.contains('c')).count()
+        print("Lines with a: %i, lines with b: %i, lines with c: %i" % (numAs, numBs, numCs))
 
-spark.stop()
+        spark.stop()
